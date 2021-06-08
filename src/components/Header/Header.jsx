b334fde79db47d058a745cb9b04ddeb1a8b1492b
Nathan <nathanbenson.affiliate@gmail.com>
2021-06-08 12:42:06 +0200

    fixed header bug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -35,7 +35,6 @@ function Header() {
             <div className="header__right">
                 <HeaderOption Icon={HomeIcon} title="Home" />
                 <HeaderOption Icon={SupervisorAccountIcon}  title="My Network" />
-                <HeaderOption Icon={SupervisorAccountIcon}  title="My Network" />
                 <HeaderOption Icon={BusinessCenterIcon}  title="Jobs" />
                 <HeaderOption Icon={ChatIcon}  title="Messaging" />
                 <HeaderOption Icon={NotificationsIcon}  title="Notifications" />
